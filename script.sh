15128cfcef5ab57847ba1785d47702e0e939d40a
Ulukele <36236949+Ulukele@users.noreply.github.com>
2021-05-20 19:30:13 +0700

    add script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+files=$(ls ../build_rep)
+for file in $files 
+do
+  rm -r ../build_rep/$file
+done
+
+cp -r ./dist/* ../build_rep
+cp -r ./models ../build_rep
+
+cd ../build_rep
+ls -a
+
+git add *; git commit -m "update"; git push
